74a828642454fbf0a3a78b12940057f06609fbf8
LUIS AUGUSTO FERREIRA <blackbarth@outlook.com>
2021-03-23 08:27:07 -0300

    validacoes avancadas

--- src/app/demos/reactiveforms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveforms/cadastro/cadastro.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormControl, Validators } from "@angular/forms";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChildren,
+} from "@angular/core";
+import {
+  FormBuilder,
+  FormControl,
+  FormControlName,
+  Validators,
+} from "@angular/forms";
 import { Usuario } from "src/app/Model/usuario";
 import { MASKS, NgBrazilValidators } from "ng-brazil";
 import { CustomValidators } from "ng2-validation";
+import {
+  DisplayMessage,
+  GenericValidator,
+  ValidationMessages,
+} from "./generic-form-validation";
 
 @Component({
   selector: "app-cadastro",
   templateUrl: "./cadastro.component.html",
   styles: [],
 })
-export class CadastroComponent implements OnInit {
+export class CadastroComponent implements OnInit, AfterViewInit {
+  //implementaçao importante para ser observada
+  @ViewChildren(FormControlName, { read: ElementRef })
+  formInputElements: ElementRef[];
+
+  validationMessages: ValidationMessages;
+  genericValidator: GenericValidator;
+  displayMessage: DisplayMessage = {};
+
   senha = new FormControl("", [
     Validators.required,
     CustomValidators.rangeLength([6, 15]),
@@ -33,8 +57,38 @@ export class CadastroComponent implements OnInit {
     senha: this.senha,
     confimarsenha: this.confimarsenha,
   });
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.validationMessages = {
+      nome: {
+        required: "Nome é de preenchimento obrigatorio",
+        minLength: "Minimo 2 caracteres",
+        maxLength: "Maximo 15 Caracteres",
+      },
+      cpf: {
+        required: "Informe o CPF",
+        cpf: "CPF em formato inválido",
+      },
+      email: {
+        required: "Informe e-mail",
+        email: "E-mail inválido",
+      },
+      senha: {
+        required: "Informe senha",
+        rangeLength: "A senha deve possuir entre 6 e 15 caracteres",
+      },
+      confimarsenha: {
+        required: "Informe senha",
+        rangeLength: "A senha deve possuir entre 6 e 15 caracteres",
+        equalTo: "As senhas não conferem",
+      },
+    };
+    this.genericValidator = new GenericValidator(this.validationMessages);
+  }
+  ngAfterViewInit(): void {
+    throw new Error("Method not implemented.");
+  }
 
+  //assim que html foi desponibilizado para o browser
   ngOnInit(): void {}
 
   adicionarUsuario() {
